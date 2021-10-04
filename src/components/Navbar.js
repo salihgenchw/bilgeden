1c32cc816b65f55cea5b914b06c9c16dc4d938dd
salihgenchw <salihgenc34@gmail.com>
2021-10-04 20:27:06 +0300

    sada

---
+++ src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Logo from "../images/bilgeden_Logo.png"
+import './Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img src={Logo} alt="Logo" className="py-2" width={170}/>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Şehir Seçiniz
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="/">
+                    İstanbul
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Ankara
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    İzmir
+                  </a>
+                </li>
+               
+              </ul>
+            </li>
+           
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
